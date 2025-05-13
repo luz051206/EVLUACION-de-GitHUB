02b2bdbc02a10a71d3230d679911fd3ce69c42a6
LENOVO <LENOVO@DESKTOP-BHJOJ0Q>
2025-05-13 14:45:14 -0600

    Agregar archivos de proyecto.

---
+++ EVLUACION de GitHUB/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EVLUACION_de_GitHUB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
